6c379275000e1a15a9b58de3b560cf90e83438b4
Whaeedil <baoppyhossen1234@gmail.com>
2024-06-13 23:19:29 +0600

    login

--- src/Components/LoginRegester/Login.jsx
+++ src/Components/LoginRegester/Login.jsx
@@ -1,7 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import GoogleLogin from "../SocialLogin/GoogleLogin";
+import auth from "../Firebase/firebase.config";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location?.state?.from?.pathname || '/'
+
+    const handleSignIn = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signInWithEmailAndPassword(email, password)
+    }
+
+    useEffect(() => {
+        if (user) {
+            navigate(from, { replace: true })
+        }
+    }, [navigate, user, from])
+    console.log(loading, error)
+
     return (
         <div>
 
@@ -13,7 +45,7 @@ const Login = () => {
 
                     </div>
                     <div className="card shrink-0 w-full max-w-lg max-h-lg shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleSignIn} className="card-body">
                             <h1 className="text-6xl font-bold text-center">Login Now !</h1>
 
                             {/* Email  */}
@@ -22,6 +54,7 @@ const Login = () => {
                                     <span className="label-text font-semibold">Email</span>
                                 </label>
                                 <input type="email"
+                                    name="email"
                                     placeholder="Email"
                                     className="input input-bordered"
                                     required />
@@ -33,6 +66,7 @@ const Login = () => {
                                     <span className="label-text font-semibold">Password</span>
                                 </label>
                                 <input type="password"
+                                    name="password"
                                     placeholder="password"
                                     className="input input-bordered"
                                     required />
